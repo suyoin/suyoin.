7f7a65f7a23ec28de12a2f3797ade3ec6afb24e2
suyoin <19378617+suyoin@users.noreply.github.com>
2021-02-13 11:04:56 -0600

    Head & AnimateSharedLayout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
+import { AnimateSharedLayout } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>suyoin</title>
+				<meta property="og:site_name" content="suyoin" />
+				<meta property="og:title" content="suyoin" />
+				<meta property="og:description" content="the place" />
+				<meta name="theme-color" content="#FF6262" />
+			</Head>
+			<AnimateSharedLayout>
+				<Component {...pageProps} />
+			</AnimateSharedLayout>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;

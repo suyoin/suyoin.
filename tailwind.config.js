4302eb4b465c162a9935f6892b76cf0d57a3b8f6
suyoin <19378617+suyoin@users.noreply.github.com>
2021-02-27 17:42:57 -0600

    fix purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-	purge: ["./src/**/*.tsx"],
+	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			fontFamily: {

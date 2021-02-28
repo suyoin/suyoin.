58041b56b564d579a97904e27431700548622818
suyoin <19378617+suyoin@users.noreply.github.com>
2021-02-27 18:41:00 -0600

    rename navbar file dynamically create tabs accept a "current" prop for the current tab

---
+++ components/NavigationBar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { useRouter, withRouter } from "next/router";
+import { useEffect } from "react";
+import pageList from "../util/pageList";
+
+const NavigationBar = ({ current }: { current: string }) => {
+	return (
+		<div className="flex flex-row justify-left items-center space-x-5 p-3 sticky w-full h-10 bg-gray-800 ">
+			<>
+				{pageList.map((pageDescription) => {
+					return (
+						<Link
+							key={pageDescription.pageName}
+							href={pageDescription.relativeUrl}
+						>
+							<a
+								className={`text-xl font-custom transition duration-300 ease-out ${
+									current === pageDescription.pageName
+										? `text-red-500`
+										: `text-gray-400 hover:text-red-400`
+								}`}
+							>
+								{pageDescription.displayName}
+							</a>
+						</Link>
+					);
+				})}
+			</>
+		</div>
+	);
+};
+
+export default NavigationBar;

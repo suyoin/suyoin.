a8c0a16fb95420746c6da3400166db5ce4446381
suyoin <19378617+suyoin@users.noreply.github.com>
2021-02-27 18:42:33 -0600

    moved Index to its own component pageList to make finding page equivalents easier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,24 @@
-import { motion } from "framer-motion";
+import Index from "../components/pages/Index";
 import Playground from "../components/pages/Playground";
 import getSubdomain from "../util/getSubdomain";
+import pageList from "../util/pageList";
+
+const pages = {
+	index: Index,
+	playground: Playground,
+};
 
 export default function IndexPage({ subdomain }): React.ReactElement {
 	if (subdomain) {
-		switch (subdomain) {
-			case "playground":
-				return <Playground />;
+		const pageDescription = pageList.find((pageDescription) => {
+			return pageDescription.pageName === subdomain;
+		});
+		if (pageDescription) {
+			return pages[pageDescription.pageName];
 		}
 	}
 
-	return (
-		<>
-			<div className="flex flex-col w-screen after-nav-bar space-y-1 items-center justify-center">
-				<motion.div
-					initial={{ opacity: 0.5, scale: 0.5 }}
-					animate={{ opacity: 1, scale: 1 }}
-					transition={{
-						opacity: { duration: 0.5 },
-						scale: { duration: 0.5 },
-					}}
-				>
-					<a
-						href="#"
-						className="text-5xl font-custom transition duration-150 ease-out text-red-400 hover:underline"
-					>
-						suyoin
-					</a>
-				</motion.div>
-				<motion.a
-					className="text-gray-400"
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{ delay: 0.5, duration: 0.5 }}
-				>
-					todo
-				</motion.a>
-			</div>
-		</>
-	);
+	return <Index />;
 }
 
 export function getServerSideProps({ req, res }) {

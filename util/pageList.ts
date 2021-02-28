69d09a7cdb195a814e3f9cc902c6ee956b09cb96
suyoin <19378617+suyoin@users.noreply.github.com>
2021-02-27 18:42:45 -0600

    pageList to make finding page equivalents easier

---
+++ util/pageList.ts
@@ -0,0 +1,12 @@
+export default [
+	{
+		displayName: "suyoin",
+		pageName: "index",
+		relativeUrl: "/",
+	},
+	{
+		displayName: "playground",
+		pageName: "playground",
+		relativeUrl: "/playground",
+	},
+];
